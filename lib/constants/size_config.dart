ffb32811e032a94b25aad6c5fc13d23c8d44f21d
yermakhan <ermahanansagan@gmail.com>
2021-05-02 13:43:41 +0600

    size Config

--- lib/constants/size_config.dart
+++ lib/constants/size_config.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 
 // использовал для media part так как картинки на большом экране съезжали с места и использовал для кнопки add friend
+// умножал на процентное соотношение виджета к экрану в процентах
 class SizeConfig {
   static MediaQueryData mediaQueryData;
   static double screenWidth;
